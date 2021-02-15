9131ddaef825e80af4561586b8aa31c4e9f2429d
Chris <cwaskom@gmail.com>
2021-02-14 19:37:24 -0500

    updated settings

--- backend/app/client.py
+++ backend/app/client.py
@@ -1,7 +1,7 @@
 import requests
+from settings import API_KEY
 
 class Client:
-    API_KEY = "38f307611e4a387b4f6a7f5976446f58"
     LAT = "41.3497"
     LON = "72.0791" 
     URL = f"https://api.openweathermap.org/data/2.5/forecast?lat={LAT}&lon={LON}&appid={API_KEY}&units=imperial"
--- backend/app/settings.py
+++ backend/app/settings.py
@@ -1,1 +1,2 @@
 RECIPIENTS = 'chris@acadiastorage.com'
+API_KEY = "38f307611e4a387b4f6a7f5976446f58"
